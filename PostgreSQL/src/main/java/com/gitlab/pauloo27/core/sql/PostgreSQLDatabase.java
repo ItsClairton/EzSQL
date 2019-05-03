6470348b4afc7bf734bd04da352ddf64adf9aaa7
Paulo <pauloeduardo2709@gmail.com>
2019-05-03 18:07:11 -0300

    🎨 Improve the Table#exists() function

--- PostgreSQL/src/main/java/com/gitlab/pauloo27/core/sql/PostgreSQLDatabase.java
+++ PostgreSQL/src/main/java/com/gitlab/pauloo27/core/sql/PostgreSQLDatabase.java
@@ -31,11 +31,18 @@ public class PostgreSQLDatabase extends Database {
     @Override
     public boolean exists() throws SQLException {
         if (!sql.isConnected()) throw new SQLException("Not connected.");
-        // TODO Change to EzTable#count when it's implemented
-        try (ResultSet result = sql.prepareStatement("SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname = '?';", getName()).executeQuery()) {
-            if (result.next())
-                return result.getInt(1) == 1;
+        // TO DO Change to EzTable#count when it's implemented
+//        try (ResultSet result = sql.prepareStatement("SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname = '?';", getName()).executeQuery()) {
+//            if (result.next())
+//                return result.getInt(1) == 1;
+//        }
+        try {
+            return sql.getTable("pg_catalog.pg_database")
+                    .count()
+                    .where().equals("datname", this.getName())
+                    .execute().getFirstColumnAsInt() == 1;
+        } catch (Exception e) {
+            return false;
         }
-        return false;
     }
 }
